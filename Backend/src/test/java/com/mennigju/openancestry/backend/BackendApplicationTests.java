74256b9f7519fd1f10c5755ada315571d51681dd
Justin Mennig <justin.mennig@ametras.com>
2024-08-05 21:11:45 +0200

    Init

---
+++ Backend/src/test/java/com/mennigju/openancestry/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mennigju.openancestry.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
